224cfdf595413f209c5aeb652919a35dce69f369
Kamaleeswari-S <princykamalikumar@gmail.com>
2024-06-22 12:28:43 +0530

    Variables-arrays-objects

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("hello world")
+
+//Simple Programs todo for variables
+var myvar=1;
+console.log("myvar");
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+var firstName = "John";
+var lastName = "Doe";
+var maritalStatus = "Single";
+var country = "USA";
+var age = 30;
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Marital Status: " + maritalStatus);
+console.log("Country: " + country);
+console.log("Age: " + age);
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+var firstName = "John", lastName = "Doe", maritalStatus = "Single", country = "USA", age = 30;
+console.log(`First Name: ${firstName}, Last Name: ${lastName}, Marital Status: ${maritalStatus}, Country: ${country}, Age: ${age}`);
+
+//Declare variables and assign string, boolean, undefined and null data types
+var str = "Hello World";       // String
+var bool = true;               // Boolean
+var undef = undefined;         // Undefined
+var nul = null;                // Null
+
+console.log(`String: ${str}, Boolean: ${bool}, Undefined: ${undef}, Null: ${nul}`);
+                          //or
+var statement1 = "I am 25 years old.";   // String
+var statement2 = "You are 30 years old."; // String
+
+var isCorrect1 = true;  // Boolean
+var isCorrect2 = false; // Boolean
+
+var notDefined = undefined; // Undefined
+
+var nothing = null; // Null
+
+console.log(`${statement1} ${isCorrect1}, ${statement2} ${isCorrect2}, ${notDefined}, ${nothing}`);
+
+
+
+
+
